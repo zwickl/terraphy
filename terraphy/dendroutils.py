3ad10abe1d03e790b2f235723acda17a6a304345
Derrick Zwickl <zwickl@email.arizona.edu>
2015-03-05 14:29:18 -0700

    part of previous commit bugfix

--- terraphy/dendroutils.py
+++ terraphy/dendroutils.py
@@ -32,12 +32,16 @@ def compat_get_taxon_set(obj):
 def compat_encode_bipartitions(tree, **kwargs):
     '''Convenience function dealing with different ways of encoding splits in DP4'''
     if hasattr(tree, "encode_bipartitions"):
-       tree.encode_bipartitions(**kwargs)
+        if 'delete_outdegree_one' in kwargs:
+            val = kwargs.pop('delete_outdegree_one')
+            kwargs['collapse_unrooted_basal_bifurcation'] = val
+        tree.encode_bipartitions(**kwargs)
     elif not hasattr(tree, "split_edges") or not tree.split_edges:
         treesplit.encode_splits(tree, **kwargs)
 
 
 def dendropy_read_treefile(treefiles, quiet=False, **kwargs):
+    out_stream = kwargs.pop('writer', sys.stderr)
     intrees = TreeList()
     if not treefiles:
         if not quiet:
@@ -53,11 +57,11 @@ def dendropy_read_treefile(treefiles, quiet=False, **kwargs):
             #try two input formats
             try:
                 if not quiet:
-                    sys.stderr.write('Reading file %s in newick format ...\n' % tf)
+                    out_stream.write('Reading file %s in newick format ...\n' % tf)
                 intrees.extend(TreeList.get_from_path(tf, "newick", case_sensitive_taxon_labels=True, preserve_underscores=True, **kwargs))
             except DataParseError:
                 if not quiet:
-                    sys.stderr.write('Reading file %s in nexus format ...\n' % tf)
+                    out_stream.write('Reading file %s in nexus format ...\n' % tf)
                 intrees.extend(TreeList.get_from_path(tf, "nexus", case_sensitive_taxon_labels=True, preserve_underscores=True, **kwargs))
             except ValueError:
                 sys.stderr.write('NOTE: ValueError reading from file %s, ' % tf)
