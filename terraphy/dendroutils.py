8207afb5f229a93b62e109d09cbd42eb330e1252
Derrick Zwickl <zwickl@email.arizona.edu>
2016-04-01 15:51:49 -0700

    improvements to treefile reading

--- terraphy/dendroutils.py
+++ terraphy/dendroutils.py
@@ -1,5 +1,6 @@
 #/usr/bin/env python
 import sys
+import os
 
 #DENDROPY PACKAGE
 try:
@@ -10,23 +11,22 @@ try:
         from dendropy.error import DataError as DataParseError
     except:
         from dendropy.utility.error import DataParseError
-    
+
+    #import dendropy.dataio.nexusreader.NotNexusFileError as NotNexusFileError
+    from dendropy.dataio.nexusreader  import NexusReader
+    from dendropy.dataio.tokenizer  import Tokenizer
+    #from  dendropy.dataio.nexusreader import NotNexusFileError
+    #from  dendropy.dataio.nexusreader import NexusReaderError
+
     #this deals with changes in DendroPy 4
     try:
         from dendropy.calculate import treesplit
     except ImportError:
         from dendropy import treesplit
 
-except ImportError:
+except ImportError as e:
+    sys.stderr.write('%s\n' % e.message)
     sys.exit('problem importing dendropy package - it is required')
-
-
-def compat_get_taxon_set(obj):
-    '''Convenience function dealing with change in Tree/TreeList member name in DP4'''
-    if hasattr(obj, 'taxon_namespace'):
-        return obj.taxon_namespace
-    else:
-        return obj.taxon_set
 
 
 def compat_encode_bipartitions(tree, **kwargs):
@@ -50,24 +50,35 @@ def dendropy_read_treefile(treefiles, quiet=False, **kwargs):
         #try two input formats
         try:
             intrees.extend(TreeList.get_from_string(trees, "nexus", case_sensitive_taxon_labels=True, preserve_underscores=True, **kwargs))
-        except DataParseError:
+        except (DataParseError, NexusReader.NotNexusFileError) as e:
+            sys.stderr.write('%s\n' % e.message)
             intrees.extend(TreeList.get_from_string(trees, "newick", case_sensitive_taxon_labels=True, preserve_underscores=True, **kwargs))
+        except (DataParseError, Tokenizer.UnexpectedEndOfStreamError, AttributeError)  as e:
+            if not quiet:
+                sys.stderr.write('%s\n' % e.message)
+                sys.exit('Could not read file %s in nexus or newick  format ...\n' % tf)
     else:
         for tf in treefiles:
+            if not os.path.isfile(tf):
+                out_stream.write('TreeFile %s  does not exist' % tf)
+                sys.exit()
+
             #try two input formats
             try:
                 if not quiet:
-                    out_stream.write('Reading file %s in newick format ...\n' % tf)
-                intrees.extend(TreeList.get_from_path(tf, "newick", case_sensitive_taxon_labels=True, preserve_underscores=True, **kwargs))
-            except DataParseError:
-                if not quiet:
                     out_stream.write('Reading file %s in nexus format ...\n' % tf)
                 intrees.extend(TreeList.get_from_path(tf, "nexus", case_sensitive_taxon_labels=True, preserve_underscores=True, **kwargs))
-            except ValueError:
-                sys.stderr.write('NOTE: ValueError reading from file %s, ' % tf)
-            except AttributeError:
-                sys.stderr.write('NOTE: AttributeError reading from file %s, ' % tf)
 
+            #except (DataParseError, dendropy.dataio.nexusreader.NotNexusFileError) as e:
+            except (DataParseError, NexusReader.NotNexusFileError, AttributeError) as e:
+                try:
+                    if not quiet:
+                        out_stream.write('Reading file %s in newick format ...\n' % tf)
+                    intrees.extend(TreeList.get_from_path(tf, "newick", case_sensitive_taxon_labels=True, preserve_underscores=True, **kwargs))
+                except (DataParseError, Tokenizer.UnexpectedEndOfStreamError, AttributeError)  as e:
+                    if not quiet:
+                        sys.stderr.write('%s\n' % e.message)
+                        sys.exit('Could not read file %s in nexus or newick  format ...\n' % tf)
     return intrees
 
 
