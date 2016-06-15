e15b4dc33cef00ab98345809af8a087ac0f71fa3
zwickl <zwickl@compute-1-2.local>
2016-06-14 21:56:26 -0700

    correction

--- scripts/terraphy.main.py
+++ scripts/terraphy.main.py
@@ -260,10 +260,10 @@ def build_or_strict_consensus(label_set, full_label_set, triplets, all_triplets,
 
         # number of resolutions in clade is possible res for that many taxa
         if annotate:
-            num_res = str(num_trees(len(label_set)))
-            valstr = "%.4G" % num_res
-            new_node.label = valstr
-            new_node.annotations.add_new('resolutions', '%s' % valstr)
+            num_res = num_trees(len(label_set))
+            valstr = "%.4g" % num_res
+            node.label = valstr
+            node.annotations.add_new('resolutions', '%s' % valstr)
 
     else:
         #This returns one component for each of the clades descending from this node
@@ -327,7 +327,7 @@ def build_or_strict_consensus(label_set, full_label_set, triplets, all_triplets,
                         #num_res = log(superb_count_parents(comp, new_trip))
                         num_res = superb_count_parents(comp, new_trip)
                         if num_res > 1:
-                            valstr = "%.4G" % num_res
+                            valstr = "%.4g" % num_res
                             new_node.label = valstr
                             new_node.annotations.add_new('resolutions', '%s' % valstr)
 
